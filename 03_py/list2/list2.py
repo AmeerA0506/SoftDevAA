51cbeec7041e8e09b66c8caeaec45f24e499f457
AmeerA0506 <aalnasser30@stuy.edu>
2022-09-21 14:05:20 -0400

    list2

--- 03_py/list2/list2.py
+++ 03_py/list2/list2.py
@@ -10,11 +10,31 @@ def count_evens(nums):
 def big_diff(nums):
     largest=nums[0]
     smallest=nums[0]
+    maxIndex = 0
+    minIndex = 0
     for x in range(len(nums)):
-        largest = max(nums[x], largest)
+        if(largest < nums[x]):
+            maxIndex = x
+            largest = nums[x]
+        else
+        if(smallest > nums[x]):
+            minIndex = x
         smallest = min(nums[x], smallest)
-    return largest-smallest
 
-#centered_average
-def centered_average():
-    
+#centered_average    
+# def centered_average(nums):
+#     largest=nums[0]
+#     smallest=nums[0]
+#     maxIndex = 0
+#     minIndex = 0
+#     
+#     for x in range(len(nums)):
+#         if(largest < nums[x]):
+#             maxIndex = x
+#             largest = nums[x]
+#             maxCopies = []
+#         elif(smallest > nums[x]):
+#             minIndex = x
+#             smallest = nums[x]
+#             
+#     
