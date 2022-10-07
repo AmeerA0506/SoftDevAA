6058fae47c3b50ebc368e29c4d07b4fa01749f6f
AmeerA0506 <aalnasser30@stuy.edu>
2022-10-07 14:06:55 -0400

    tears

--- 08_serve/app.py
+++ 08_serve/app.py
@@ -5,6 +5,7 @@ K07 -- Web dev *
 2022-10-4
 time spent: 0.5hrs
 '''
+import csv
 import random as rng
 from flask import Flask
 app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs?
@@ -16,6 +17,7 @@ def occupation():
     sha= file1.readlines()
     sha = sha[:-1] # we want to remove the last entry, which is the total 
     dictionary = {}
+    output=""
     for e in sha:
         if e[0] == '"':
             dictionary[e[1:e.find('",')]] = float(e[e.find('",')+2:len(e)-1]) # if theres a comma in the key, we want to acknowledge it as a dif situation 
@@ -29,27 +31,16 @@ def occupation():
         for x in range(number):
             occlist.append(key) # we wanted to find a way to make it random
     index = rng.randint(0, len(occlist)-1)
-    return "Your randomly selected occupation is " + occlist[index] + " with a " + str(dic[occlist[index]]) + "% chance." + , occupations.csv
+    
+    
+    for x in dic:
+        output+= x + "<br>"
+    return "<h2> Young Lions Crying At War:: Ameer Alnasser, Yat Long Chan, Wilson Mach</h2>"\
+           +"Your randomly selected occupation is " + occlist[index] \
++ " with a " + str(dic[occlist[index]]) + "% chance." + "<br>"+"<br>"\
++"<h1> Occupations </h1>" + output
         
+app.debug = True        # enable auto-reload upon code change, auto reloading the terminal and not the webpage
 
 app.run()  # Q5: Where have you seen similar constructs in other languages?
 
-
-'''
-DISCO:
-*   If two functions are defined consecutively, only the first one will be run
-python3 app.py -> typing hte link given into the web browser gives us the output of the function written without needing (print(hello_world())
-*   127.0.0.1 is our home directory, only local
-*   Flask is installed only if package is installed
-*   if ctrl C is hit, 127.0.0.1 is unreachable
-
-QCC:
-0.  Line 12 utilized a similar syntax as the java constructor
-1.  the forward slash is utilized when we navigate directories
-2.  the output will be printed inside of the directory with the app, with the #3 name from the constructor
-4.  It will  appear on the webpage of the route
-5.  java also has object-oriented programming, as seen on line 21 wih the call of a method from app  
-...
-INVESTIGATIVE APPROACH:
-Our team jumped to running the command, then working it out backwards. We did not realize the URL until QAF traffic (before the team shuffle)
-'''
