bddf9c41edb410bff75ae260ca2d7b4551ce8708
Ameer Alnasser <aalnasser30@stuy.edu>
2022-10-17 15:25:55 -0400

    Update app.py

--- 11_flask-forms/app.py
+++ 11_flask-forms/app.py
@@ -29,34 +29,36 @@ PROTIP: Insert your own in-line comments
    understand what is going on.
 '''
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def disp_loginpage():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
     print(app) #prints the app running the request (Flask)
     print("***DIAG: request obj ***")
     print(request) #prints the server that app.py is running on
-    print("***DIAG: request.args ***")
-    print(request.args) #returns the dictionary in which the args are stored
-    # print("***DIAG: request.args['username']  ***")
-    # print(request.args['username']) #code returns an error when ran because arg
+    print("***DIAG: request.form ***")
+    #print(request.args) #returns the dictionary in which the args are stored
+    print(request.form)
+    #print("***DIAG: request.form['username']  ***")
+    #print(request.form['username']) #code returns an error when ran because arg
                                       #argument requested doesn't exist
     print("***DIAG: request.headers ***")
     print(request.headers) # returns data about the webpage (operating system, web browser, etc.)
     return render_template( 'login.html' ) #run website with template
 
 
-@app.route("/auth", methods=['GET', 'POST'])
+@app.route("/auth", methods=['POST'])
 def authenticate():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
     print(app)
     print("***DIAG: request obj ***")
     print(request)
-    print("***DIAG: request.args ***")
-    print(request.args)
-    # print("***DIAG: request.args['username']  ***")
-    # print(request.args['username']) #returns username entered
+    print("***DIAG: request.form ***")
+    #print(request.args)
+    print(request.form)
+    print("***DIAG: request.form['username']  ***")
+    print(request.form['username']) #returns username entered
     print("***DIAG: request.headers ***")
     print(request.headers)
     return "Waaaa hooo HAAAH"  #response to a form submission
