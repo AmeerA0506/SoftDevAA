250123005c59a4f0bc268ca4c132cc4ebfda6f33
AmeerA0506 <aalnasser30@stuy.edu>
2022-11-03 14:06:03 -0400

    work

---
+++ 19_session/app.py
@@ -0,0 +1,95 @@
+# Clyde 'Thluffy' Sinclair
+# SoftDev
+# Oct 2022
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+#the conventional way:
+#from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+
+username="Kwa"
+password="kwa"
+'''
+trioTASK:
+~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
+...read for understanding all of the code below.
+Some will work as written; other sections will not. 
+TASK: Predict which...
+Devise some simple tests you can run to "take apart this engine," as it were.
+Execute your tests.
+Process results.
+
+PROTIP: Insert your own in-line comments
+ wherever they will help
+  your future self and/or current teammates
+   understand what is going on.
+'''
+from flask import session
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+@app.route("/") #, methods=['GET', 'POST'])
+def disp_loginpage():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    return render_template( 'login.html' )
+
+
+@app.route("/auth") # , methods=['GET', 'POST'])
+def authenticate():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    return "Waaaa hooo HAAAH"  #response to a form submission
+
+
+    
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
