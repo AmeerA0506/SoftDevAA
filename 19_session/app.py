5b95932222a353fe78f0c1b862c628420adb17c0
AmeerA0506 <aalnasser30@stuy.edu>
2022-11-07 13:36:10 -0500

    oops

--- 19_session/app.py
+++ 19_session/app.py
@@ -1,6 +1,10 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# Oct 2022
+"""
+Foo Bar:: Ameer Alnasser, Wan Ying Li, Kevin Wang
+SoftDev
+k19 -- username/password authentication
+2022-11-05
+time spent: 1.0 hrs
+"""
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
@@ -11,23 +15,9 @@ from flask import request           #facilitate form submission
 
 app = Flask(__name__)    #create Flask object
 
-CORRECT_username="Kwa"
-CORRECT_password="kwa"
-'''
-trioTASK:
-~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
-...read for understanding all of the code below.
-Some will work as written; other sections will not. 
-TASK: Predict which...
-Devise some simple tests you can run to "take apart this engine," as it were.
-Execute your tests.
-Process results.
+CORRECT_username="lol"
+CORRECT_password="lol"
 
-PROTIP: Insert your own in-line comments
- wherever they will help
-  your future self and/or current teammates
-   understand what is going on.
-'''
 from flask import session, redirect, url_for
 
 # Set the secret key to some random bytes. Keep this really secret!
@@ -35,25 +25,32 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def index():
-    if 'username' in session:
-        return render_template("home.html", username=session["username"], logged_in=True)
+    if 'username' in session: #ensure the user isn't already signed in
+        #to avoid KeyError if session["username"] doesn't exist
+        return render_template("response.html", username=session["username"], logged_in=True)
     else:
-        return render_template("home.html", logged_in=False)
+        return render_template("response.html", logged_in=False)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        if (CORRECT_username == request.form["username"] and CORRECT_password == request.form["password"]):
+    if request.method == 'POST': #when submitting the form
+        #username is case-unsensitive and password is case-sensitive
+        if (CORRECT_username.lower() == request.form["username"].lower() and CORRECT_password == request.form["password"]): #username/password auth
             session['username'] = request.form['username']
             return redirect(url_for('index'))
+        #when username/password combo is wrong
+        elif (CORRECT_username.lower() != request.form["username"].lower()):
+            return render_template("login.html", username = request.form["username"], error_message="user doesn't exist")
+        elif (CORRECT_username.lower() == request.form["username"].lower() and CORRECT_password != request.form["password"]):
+            return render_template("login.html", username = request.form["username"], error_message="incorrect password")
         else:
-            return "wrong username or password"
+            return render_template("login.html", username = request.form["username"], error_message="juju madness")  
         
-    else:
-        if 'username' in session:
+    else: #when accessing /login for the first time
+        if 'username' in session: #ensure the user isn't already signed in
             return redirect(url_for('index'))
         else:
-            return render_template("login.html", logged_in=False)
+            return render_template("login.html")
 
         
 
@@ -63,39 +60,6 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
-"""
-@app.route("/") #, methods=['GET', 'POST'])
-def disp_loginpage():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    return render_template( 'login.html' )
-
-
-@app.route("/auth") # , methods=['GET', 'POST'])
-def authenticate():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    return "Waaaa hooo HAAAH"  #response to a form submission
-
-"""
     
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
