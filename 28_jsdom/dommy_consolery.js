c450e872e0e36ede9bf2150b7bb7975acdf21882
Ameer Alnasser <aalnasser30@stuy.edu>
2023-04-16 21:22:54 -0400

    Update dommy_consolery.js

--- 28_jsdom/dommy_consolery.js
+++ 28_jsdom/dommy_consolery.js
@@ -1,64 +1,37 @@
-
-// Team Casa Blanca :: Ameer Alnasser, Sam Cowan
+// Team Casa Blanca -- Sam Cowan, Ameer Alnasser
 // SoftDev pd2
-// K28 -- Basic functions in JavaScript
-// 2023-04-05w
-//
-
-/*
-   your PPTASK:
-   
-   Test drive each bit of code in this file,
-    and insert comments galore, indicating anything
-     you discover,
-    	have questions about,
-    		or otherwise deem notable.
-    		
-    		Write with your future self or teammates in mind.
-    		
-    		If you find yourself falling out of flow mode, consult 
-    		other teams
-    		MDN
-
-   A few comments have been pre-filled for you...
-   
-   (delete this block comment once you are done)
-*/
-
-// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
-// SoftDev pd0
-// K28 -- Getting more comfortable with the dev console and the DOM
-// 2023-04-05w
+// K28 -- Manipulating the DOM
+// 2023-04-17
 // --------------------------------------------------
 
 
 //send diagnostic output to console
 //(Ctrl-Shift-K in Firefox to reveal console)
-console.log("AYO");
+console.log("AYO"); //Gets printed before you do anything
 
-var i = "hello";
+var i = "hello"; //typing i or j in the terminal will return these
 var j = 20;
 
 
 //assign an anonymous fxn to a var
-var f = function(x) {
+var f = function(x) { //adds 30 to input
   var j=30;
   return j+x;
 };
 
 
 //instantiate an object
-var o = { 'name' : 'Thluffy',
-          age : 1024,
-          items : [10, 20, 30, 40],
+var o = { 'name' : 'Thluffy', //Can do o["name"] in dev console to return
+          age : 1024, //needs quotes
+          items : [10, 20, 30, 40], //o["items"][1] returns 20
           morestuff : {a : 1, b : 'ayo'},
-          func : function(x) {
+          func : function(x) { //o["func"](7) runs the function
             return x+30;
           }
         };
 
 
-var addItem = function(text) {
+var addItem = function(text) { //adds item to list on webpage
   var list = document.getElementById("thelist");
   var newitem = document.createElement("li");
   newitem.innerHTML = text;
@@ -66,13 +39,13 @@ var addItem = function(text) {
 };
 
 
-var removeItem = function(n) {
+var removeItem = function(n) { //removes item index n from list
   var listitems = document.getElementsByTagName('li');
   listitems[n].remove();
 };
 
 
-var red = function() {
+var red = function() { //makes the first and last thing on the list red
   var items = document.getElementsByTagName("li");
   for(var i = 0; i < items.length; i++) {
     items[i].classList.add('red');
@@ -80,7 +53,7 @@ var red = function() {
 };
 
 
-var stripe = function() {
+var stripe = function() { //makes the first red, all else blue
   var items = document.getElementsByTagName("li");
   for(var i = 0; i < items.length; i++) {
     if (i%2==0){
@@ -92,7 +65,6 @@ var stripe = function() {
 };
 
 //insert your implementations here for...
-// FIB
 function fib(n){
   if(n === 0){
     return 0;
@@ -101,8 +73,8 @@ function fib(n){
     return 1;
   }
   return fib(n-1)+fib(n-2);
-} 
-// FAC
+}
+
 function fact(n){
   if(n < 2){
     return 1;
@@ -110,17 +82,36 @@ function fact(n){
   return (n*fact(n-1));
 }
 
-// GCD
-function GCD(n){
-  GCD=sqrt(n);
+function gcd(a,b){
+  if (a > b){
+    if (a % b === 0){
+      return b;
+    }
+    return gcd(b, a%b);
+  }
+  if (a < b){
+    if (b % a === 0){
+      return a;
+    }
+    return gcd(a, b%a);
+  }
+  return a;
 }
 
 // In addition to the style shown above,
 //  you are encouraged to test drive the "arrow function syntax" as shown below.
 //  Note anything notable.
-const myFxn = (param1, param2) => {
-  // body
-  return retVal;
+const myFxn = (a, b) => {
+  addItem("a: " + a);
+  addItem("b: " + b);
+  addItem("gcd(a,b): " + gcd(a,b));
+  addItem("fact(a): " + fact(a));
+  addItem("fib(b): " + fib(b));
+
+  return "successssssss";
 };
 
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
